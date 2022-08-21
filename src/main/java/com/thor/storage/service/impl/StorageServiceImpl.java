1056af18f6c0e5671c4f5a3afa36e169637bf078
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2022-08-21 13:03:35 -0300

    ajuste de objeto

--- src/main/java/com/thor/storage/service/impl/StorageServiceImpl.java
+++ src/main/java/com/thor/storage/service/impl/StorageServiceImpl.java
@@ -39,7 +39,7 @@ public class StorageServiceImpl implements StorageService {
     private BlobClientBuilder client;
 
     @Override
-    public String insert(List<MultipartFile> files)  {
+    public String insert(List<MultipartFile> files) {
         var document = this.repository.save(StorageFileDocument.builder().build());
         this.uploadFile(files, document);
         return document.getId();
@@ -48,11 +48,11 @@ public class StorageServiceImpl implements StorageService {
     @Override
     public StorageResponse getById(String id) {
         var storage = findById(id);
-        if(storage.getFinalUploadDate() == null)
+        if (storage.getFinalUploadDate() == null)
             throw new BusinessException(UPLOAD_NOT_FINISHED, PROCESSING);
 
         var response = StorageBuilder.toResponse(storage);
-        if(storage.getFile() != null)
+        if (storage.getFile() != null)
             return response.files(getFileResponses(storage)).build();
 
         return response.file(getSingleFileResponse(storage)).build();
@@ -80,7 +80,7 @@ public class StorageServiceImpl implements StorageService {
     @Override
     public void delete(String id) {
         var storage = findById(id);
-        if(storage.getFile() != null)
+        if (storage.getFile() != null)
             storage.getFiles().stream().forEach(file -> deleteAzure(file.getAzureId()));
         else
             deleteAzure(storage.getFile().getAzureId());
@@ -93,41 +93,38 @@ public class StorageServiceImpl implements StorageService {
 
     @Async
     private void uploadFile(List<MultipartFile> files, StorageFileDocument document) {
-        files.stream().forEach(file -> {
-            try {
+
+        if (!files.isEmpty() && files.size() == 1) {
+            var file = files.get(0);
+            FileDocument fileDocument = saveFileAzure(file);
+            document.setFile(fileDocument);
+        } else {
+            files.stream().forEach(file -> {
                 FileDocument fileDocument = saveFileAzure(file);
                 document.getFiles().add(fileDocument);
-
-            } catch (IOException e) {
-                log.error(format(AZURE_ERROR, file.getOriginalFilename(), e.getMessage()));
-            }
-        });
+            });
+        }
         document.setFinalUploadDate(now());
         this.repository.save(document);
     }
 
-    @Async
-    private void uploadFile(MultipartFile file, StorageFileDocument document) {
-            try {
-                document.setFile(saveFileAzure(file));
-                document.setFinalUploadDate(now());
-                this.repository.save(document);
-            } catch (IOException e) {
-                log.error(format(AZURE_ERROR, file.getOriginalFilename(), e.getMessage()));
-            }
-    }
-
-    private FileDocument saveFileAzure(MultipartFile file) throws IOException {
+    private FileDocument saveFileAzure(MultipartFile file)  {
+        try {
         var fileDocument = FileDocument.builder()
                 .azureId(UUID.randomUUID().toString())
                 .originalName(file.getOriginalFilename())
                 .contentType(file.getContentType())
                 .build();
 
-        this.client.blobName(fileDocument.getAzureId())
-                .buildClient()
-                .upload(file.getInputStream(), file.getSize());
-        return fileDocument;
+
+            this.client.blobName(fileDocument.getAzureId())
+                    .buildClient()
+                    .upload(file.getInputStream(), file.getSize());
+            return fileDocument;
+        } catch (IOException e) {
+            log.error(format(AZURE_ERROR, file.getOriginalFilename(), e.getMessage()));
+        }
+        return null;
     }
 
     private StorageFileDocument findById(String id) {
